a14e9b1c7a9fd1e1bff4e9f12488a46cad37a2a1
Nycolas R. Alves <nycolasramon3@gmail.com>
2025-11-06 00:34:48 -0300

    [feat] Seção de calendário

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import { Button } from './components/Button';
-import { Heading } from './components/Heading';
+import CalendarFeature from './components/CalendarFeature';
 import './index.css';
 
 function App() {
   return (
-    <>
-      <Heading>Hello World of Heading</Heading>
-      <Heading type='h2' color='white'>Hello World of Heading 2</Heading>
-      <Button>Botão 1</Button>
-      <Button variant='secondary'>Botão 2</Button>
-      <Button isBold>Botão 3</Button>
-      <Button variant='secondary' isBold>Botão 4</Button>
-    </>
+    <div>
+      <CalendarFeature />
+    </div>
   )
 }
 
--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -1,9 +1,13 @@
-export const Heading = ({ children, type = 'h1', color = '' }) => {
+export const Heading = ({ children, type = 'h1', color = '', className='' }) => {
   const customizedColor = color === 'white' ? `text-white` : 'text-gray-900';
 
   if (type === 'h1') {
-    return <h1 className={`text-[4.625rem] leading-21 font-semibold ${customizedColor}`}>{children}</h1>
+    return <h1 className={`text-[4.625rem] leading-21 font-semibold ${customizedColor} ${className}`}>{children}</h1>
   }
 
-  return <h2 className={`text-5xl leading-13.75 ${customizedColor}`}>{children}</h2>
+  if (type === 'h4') {
+    return <h1 className={`text-[1.75rem] leading-10 font-normal ${customizedColor} ${className}`}>{children}</h1>
+  }
+  
+  return <h2 className={`text-5xl leading-13.75 ${customizedColor} ${className}`}>{children}</h2>
 }
